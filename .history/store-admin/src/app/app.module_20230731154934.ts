cb5104af0cf7100190de033e877c017fa38294c6
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2023-07-31 16:04:23 -0500

    First setup for the angular admin project

---
+++ .history/store-admin/src/app/app.module_20230731154934.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NbThemeModule } from '@nebular/theme';
+import { MyComponentComponent } from './my-component/my-component.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import { CustomersComponent } from './pages/customers/customers.component';
+import { ReportsComponent } from './pages/reports/reports.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [AppComponent, MyComponentComponent, DashboardComponent, ProductsComponent, CategoriesComponent, OrdersComponent, CustomersComponent, ReportsComponent],
+  imports: [BrowserModule, AppRoutingModule, N, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
